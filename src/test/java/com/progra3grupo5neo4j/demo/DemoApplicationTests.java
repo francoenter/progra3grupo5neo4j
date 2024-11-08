e8c5b5533f589ead43923aa61d4525caea95eaed
francoenter <55357025+francoenter@users.noreply.github.com>
2024-11-07 21:55:07 -0300

    Add files via upload

---
+++ src/test/java/com/progra3grupo5neo4j/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra3grupo5neo4j.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
